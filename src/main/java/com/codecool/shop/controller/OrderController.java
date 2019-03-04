9083ad52fb63062188481933e42932e163a5e9e8
NorbertPap <garrote713@gmail.com>
2019-03-04 10:28:05 +0100

    Moved memory implementations to different package

--- src/main/java/com/codecool/shop/controller/OrderController.java
+++ src/main/java/com/codecool/shop/controller/OrderController.java
@@ -2,8 +2,8 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.ShoppingCartDao;
-import com.codecool.shop.dao.implementation.OrderDaoMem;
-import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
+import com.codecool.shop.dao.implementation.memory.OrderDaoMem;
+import com.codecool.shop.dao.implementation.memory.ShoppingCartDaoMem;
 import com.codecool.shop.model.Address;
 import com.codecool.shop.model.Order;
 import com.codecool.shop.model.PersonalInfo;
