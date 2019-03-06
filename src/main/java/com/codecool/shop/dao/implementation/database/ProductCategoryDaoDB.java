abbc24bace2269fbfe1d5ce35f9f5b217afbd99c
Zoli <zoltan.konradi@gmail.com>
2019-03-06 14:38:01 +0100

    Dumped data to csv.

--- src/main/java/com/codecool/shop/dao/implementation/database/ProductCategoryDaoDB.java
+++ src/main/java/com/codecool/shop/dao/implementation/database/ProductCategoryDaoDB.java
@@ -1,6 +1,7 @@
 package com.codecool.shop.dao.implementation.database;
 
 import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 
 import java.util.ArrayList;
@@ -24,21 +25,30 @@ public class ProductCategoryDaoDB extends DaoDatabase implements ProductCategory
         values.add(category.getName());
         values.add(category.getDescription());
         values.add(category.getDepartment());
-        executeQuery("INSERT INTO Suppliers (name, description) VALUES (?, ?);", values);
+        executeQuery("INSERT INTO product_categories (name, description, department) VALUES (?, ?, ?);", values);
     }
 
     @Override
     public ProductCategory find(int id) {
-        return null;
+        List<Object> values = new ArrayList<>();
+        values.add(id);
+        return (ProductCategory) executeQuery("SELECT * FROM product_categories WHERE id=?;", values).get(0);
     }
 
     @Override
     public void remove(int id) {
-
+        List<Object> values = new ArrayList<>();
+        values.add(id);
+        executeQuery("DELETE FROM product_categories WHERE id=?;", values);
     }
 
     @Override
     public List<ProductCategory> getAll() {
-        return null;
+        List<Object> values = executeQuery("SELECT * FROM product_categories;", null);
+        List<ProductCategory> categoryListList = new ArrayList<>();
+        for (Object value: values) {
+            if(value instanceof ProductCategory) categoryListList.add((ProductCategory)value);
+        }
+        return categoryListList;
     }
 }
