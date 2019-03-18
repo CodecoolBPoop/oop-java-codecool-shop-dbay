32c1228665647016f3bc4082014f04a2d4ededa8
NorbertPap <garrote713@gmail.com>
2019-03-18 10:21:58 +0100

    Test changed

--- src/tests/com/codecool/shop/dao/implementation/database/OrderDaoDBTest.java
+++ src/tests/com/codecool/shop/dao/implementation/database/OrderDaoDBTest.java
@@ -1,12 +1,14 @@
 package com.codecool.shop.dao.implementation.database;
 
+import com.codecool.shop.dao.OrderDao;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderDaoDBTest {
     @Test
-    public void testWhatIsAddressDaoDBsFullClassName(){
-        OrderDaoDB orderDaoDB = OrderDaoDB.getInstance();
+    public void testIsGettingOrderReturnsExistingOrder(){
+        OrderDao orderDao = OrderDaoDB.getInstance();
+        orderDao.getOrder("0");
     }
 }
